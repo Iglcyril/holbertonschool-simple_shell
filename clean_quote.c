92dbe69ffe1b40eddfd55c2d391123eca9a2900a
Iglcyril <iglcyril@hotmail.fr>
2025-12-17 17:22:01 +0000

    fix: error betty style

--- clean_quote.c
+++ clean_quote.c
@@ -7,31 +7,31 @@
  */
 char *clean_quotes(char *token)
 {
-    size_t token_len;
+	size_t token_len;
 
-    if (token == NULL)
-        return (token);
+	if (token == NULL)
+		return (token);
 
-    token_len = strlen(token);
+	token_len = strlen(token);
 
-    if (token_len > 1)
-    {
-        if (token[0] == '"')
-        {
-            if (token[token_len - 1] == '"')
-            {
-                token[token_len - 1] = '\0';
-                return (token + 1);
-            }
-        }
-        if (token[0] == '\'')
-        {
-            if (token[token_len - 1] == '\'')
-            {
-                token[token_len - 1] = '\0';
-                return (token + 1);
-            }
-        }
-    }
-    return (token);
+	if (token_len > 1)
+	{
+		if (token[0] == '"')
+		{
+			if (token[token_len - 1] == '"')
+			{
+				token[token_len - 1] = '\0';
+				return (token + 1);
+			}
+		}
+		if (token[0] == '\'')
+		{
+			if (token[token_len - 1] == '\'')
+			{
+				token[token_len - 1] = '\0';
+				return (token + 1);
+			}
+		}
+	}
+	return (token);
 }
