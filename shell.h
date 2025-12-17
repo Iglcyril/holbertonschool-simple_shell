5b3eb36ed612a54754cf74f3f9196e4894be04dc
nobulp <lucaslupon873@gmail.com>
2025-12-17 15:49:14 +0000

    fix: update shell.h prototypes

--- shell.h
+++ shell.h
@@ -12,7 +12,7 @@ extern char **environ;
 void strip_newline(char *input_str, ssize_t *str_len);
 int main(int argc, char **argv);
 int exec_cmd(char *cmd);
-int token_count(const char *input_str, const char *delim)
+int token_count(const char *input_str, const char *delim);
 
 
 #endif
