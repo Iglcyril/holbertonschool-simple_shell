023dff51c4327ec816df04344ffa24b1b62fc04c
Iglcyril <iglcyril@hotmail.fr>
2025-12-17 12:18:50 +0000

    feat: add .h file with prototypes

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+void strip_newline(char *input_str, ssize_t *str_len);
+int main(int argc, char **argv);
+int exec_cmd(char *cmd);
+
+
+#endif
