2cd0d04dde3b6e6e7414bc8460161e8a1d5e35fe
Iglcyril <iglcyril@hotmail.fr>
2025-12-18 13:19:11 +0000

    feat: create a basic build path file test OK /bin/ls

---
+++ all_for_path.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+/**
+* build_path - Constructs a complete file path by combining a directory path and a command name.
+* @dir_path: The directory path.
+* @cmd_name: The command name.
+* Return: Pointer to the newly constructed complete file path string.
+*/
+char *build_path(char *dir_path, char *cmd_name)
+{
+	char *complet_path;
+	size_t total_len;
+
+	total_len = strlen(dir_path) + strlen(cmd_name);
+	complet_path = malloc(total_len);
+
+	strcpy(complet_path, dir_path);
+	strcat(complet_path, "/");
+	strcat(complet_path, cmd_name);
+
+	return (complet_path);
+}
+
