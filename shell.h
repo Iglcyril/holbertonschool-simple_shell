b50541152f733b872efb61313d5cfc1dd5b8376b
Iglcyril <iglcyril@hotmail.fr>
2025-12-20 08:23:43 +0000

    feat: fix betty error line too long

--- shell.h
+++ shell.h
@@ -9,13 +9,15 @@
 #include <string.h>
 
 char *build_path(char *dir_path, char *cmd_name);
-int search_in_path(char **path_dirs, char **cmd_args, char **envp, char *prog_name);
+int search_in_path(char **path_dirs, char
+	**cmd_args, char **envp, char *prog_name);
 int exec_direct(char **cmd_args, char **envp, char *prog_name);
 int find_and_execute(char **cmd_args, char **envp, char *prog_name);
 
 void builtin_env(char **envp);
 void builtin_exit(int *exit_status, char *input_line);
-int check_builtin(char **cmd_tokens, int *exit_status, char *input_line, char **envp);
+int check_builtin(char **cmd_tokens, int *exit_status,
+	char *input_line, char **envp);
 
 char *clean_quotes(char *token);
 
